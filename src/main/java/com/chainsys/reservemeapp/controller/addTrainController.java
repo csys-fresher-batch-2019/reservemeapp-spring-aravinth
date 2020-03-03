da022b37c9d70695adde6d11e203d5be620b68a6
Aravinth P <aravinthp1999@gmail.com>
2020-03-03 14:50:33 +0530

    updates

--- src/main/java/com/chainsys/reservemeapp/controller/addTrainController.java
+++ src/main/java/com/chainsys/reservemeapp/controller/addTrainController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import trainticket.AdminRole.AdminListOfTrains;
@@ -11,21 +12,32 @@ import trainticket.AdminRole.AdminListOfTrainsDAO;
 import trainticket.AdminRole.AdminListOfTrainsImpl;
 import trainticket.Exception.DbException;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api")
 public class addTrainController {
 	@PostMapping("/addtrains")
 	public void addTrains(@RequestBody AdminListOfTrains l) {
-	AdminListOfTrainsDAO dao =new  AdminListOfTrainsImpl ();
-	 try {
-		// System.out.println(l);
-		dao.addTrains(l);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		AdminListOfTrainsDAO dao = new AdminListOfTrainsImpl();
+		try {
+			// System.out.println(l);
+			dao.addTrains(l);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	
+	@PostMapping("/updatetraintimings")
+	public void updateTiming(@RequestParam("trainnum") int trainnum,
+			@RequestParam("travellingTime") String travellingtime) {
+		AdminListOfTrainsDAO dao = new AdminListOfTrainsImpl();
+		try {
+			dao.updateTimings(trainnum, travellingtime);
+		} catch (DbException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
