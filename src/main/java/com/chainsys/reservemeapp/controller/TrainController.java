c76324f9f58a87d7d648d804db92fda454daefd7
Aravinth P <aravinthp1999@gmail.com>
2020-03-03 12:44:40 +0530

    updates

--- src/main/java/com/chainsys/reservemeapp/controller/TrainController.java
+++ src/main/java/com/chainsys/reservemeapp/controller/TrainController.java
@@ -33,4 +33,6 @@ public class TrainController {
 		}
 		return list;
 	}
+	
 }
+
---
+++ src/main/java/com/chainsys/reservemeapp/controller/addTrainController.java
@@ -0,0 +1,31 @@
+package com.chainsys.reservemeapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import trainticket.AdminRole.AdminListOfTrains;
+import trainticket.AdminRole.AdminListOfTrainsDAO;
+import trainticket.AdminRole.AdminListOfTrainsImpl;
+import trainticket.Exception.DbException;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("api")
+public class addTrainController {
+	@PostMapping("/addtrains")
+	public void addTrains(@RequestBody AdminListOfTrains l) {
+	AdminListOfTrainsDAO dao =new  AdminListOfTrainsImpl ();
+	 try {
+		// System.out.println(l);
+		dao.addTrains(l);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+
+	
+}
